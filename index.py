29c245f4cc283e68097f10299335dea15bbd1afb
tarek <tareksaier@gmail.com>
2015-01-09 09:18:07 +0100

    working on blog

--- index.py
+++ index.py
@@ -6,6 +6,8 @@ import cgitb
 cgitb.enable()
 
 import codecs
+import json.decoder # why OVH? why?!
+import json.encoder
 import re
 import sys
 
@@ -26,6 +28,8 @@ navitems=[{'link':'tl_dr','text':'tl;dr'},
           {},
           {},
           {'link':'contact','text':'contact'}]
+jsdec = json.decoder.JSONDecoder()
+jsenc = json.encoder.JSONEncoder()
 
 # markdown v2.5 dropped support for python 2.6, OVH uses python 2.6.6
 # also, including modules w/o installing because hosting contract only
@@ -41,34 +45,70 @@ def yt_toggles(markdown):
     yt_toggle = re.compile('^<!-- ytdd:(.*):(.*) -->$', re.M)
     return re.sub(yt_toggle, r'<label for="vis-toggle-\2">\1</label><br>'\
         r'<input type="checkbox" id="vis-toggle-\2"/>'\
-        r'<iframe id="vis-content-\2" width="700" height="436" '\
+        r'<iframe id="vis-content-\1" width="700" height="436" '\
         r'src="//www.youtube.com/embed/\2" frameborder="0" allowfullscreen>'\
         r'</iframe>', markdown)
 
+def blog_entry(e, imgside):
+    content = u'# [{0}](?a={1})\n'.format(e['headline'], e['id'])
+    content +=  u'<div class="imgfloat{0}">'\
+                    u'<img src="static/img/blog/{1}">'\
+                    u'<div class="imgsrc"><p>'\
+                        u'<a href="static/img/blog/{2}">source</a>'\
+                    u'</p></div>'\
+                u'</div>\n'.format(imgside, e['image'], 'source')
+    content += u'{0}\n'.format(e['text'])
+    content += u'<div class="footline">'\
+                u'<div class="tags"><p><strong>tags: {0}</strong></p></div>'\
+                u'<div class="date"><p>{1}</p></div>'\
+               u'</div>'.format(' '.join(e['tags']), e['date'])
+    return content
+
 def blog_entries(postget):
-    #TODO implement
+    perma = None
+    tag   = None
+    page  = 1
 
     # read entries from json file
+    fd = codecs.open('static/blog/entries.json', encoding='utf-8')
+    jsn = fd.read()
+    fd.close()
+    entries_u = jsdec.decode(jsn)
+    entries = sorted(entries_u, key=lambda e: e['date'], reverse=True)
+
     # handle permalinks, tags, pages
+    if 'a' in postget:
+        perma = postget['a'].value
+    if 't' in postget:
+        tag   = postget['t'].value
+    if 'p' in postget:
+        page  = postget['p'].value
     # return apprpriate subset
-    return md_entries
+
+    content = blog_entry(entries[0], 'left')
+    content += '\n- - -\n'
+    content += blog_entry(entries[1], 'right')
+    content += '\n- - -\n'
+    content += blog_entry(entries[2], 'left')
+    #return md_entries
+    return content
 
 # - - - - - - - - - -
 
 env = Environment(loader=FileSystemLoader('static/templates'))
 postget = cgi.FieldStorage()
 if not 'c' in postget:
-    page = 'person'
+    page = 'blog'
 else:
     page = postget['c'].value
     valid = [itm['link'] for itm in navitems if 'link' in itm]
     if not page in valid:
         page = 'notfound'
 
-if page = 'blog':
+if page == 'blog':
     content = blog_entries(postget)
 else:
-    fd = codecs.open('static/content/{0}.md'.format(page), encoding='utf-8')
+    fd = codecs.open('static/pages/{0}.md'.format(page), encoding='utf-8')
     content = fd.read()
     fd.close()
 
